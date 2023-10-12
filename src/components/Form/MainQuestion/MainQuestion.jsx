b4113c9122c424ef4d05a6afc6af4bbfd95d0bff
Piskizhev <zybrec07@gmail.com>
2023-10-12 09:25:25 +0200

    init

---
+++ src/components/Form/MainQuestion/MainQuestion.jsx
@@ -0,0 +1,40 @@
+import "../css/Questions.css";
+import bin from "../../../images/icons/bin.svg";
+import React, { useState } from "react";
+import AnswerType from "./Answers/AnswerType";
+
+function QuestionMain() {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className='question'>
+      <div className='questionLable'>
+        <div className='questionNumber'>#'$question_number' Question</div>
+        <div className='questionBin'>
+          <a href='/sport-form'>
+            <img src={bin} alt='delete' />
+          </a>
+        </div>
+      </div>
+      <div className='questionTitle'>
+        <div id='textInput' type='textInput'>
+          Question title
+        </div>
+        <div className='questionInput'>
+          <input
+            type='text'
+            name='titleInput'
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </div>
+        <AnswerType />
+      </div>
+    </div>
+  );
+}
+
+export default QuestionMain;
